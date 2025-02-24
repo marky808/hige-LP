9736085493225b2511e24eb395926ec624c7002c
Iram Shahzad <iramshahzad@yahoo.com>
2025-02-24 14:48:58 +0600

    fixed type quiz point

--- src/pages/PersonalTypeQuiz.jsx
+++ src/pages/PersonalTypeQuiz.jsx
@@ -6,96 +6,102 @@ const questions = [
   {
     category: "家族",
     text: "現在ご結婚されてますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "家族",
     text: "未成年のお子さんはいらっしゃいますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "家族",
     text: "あなたの収入がないと家計が成り立ちませんか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "家族",
     text: "自分にもしものことがあった時、家族の生活が不安ですか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "家族",
     text: "自宅はあなた名義の持ち家ですか？",
-    points: { yes: 7, no: 0 } },
+    points: { yes: 1, no: 0 }
+  },
   {
     category: "資金",
     text: "現在500万以上の資産はありますか？",
-    points: { yes: 7, no: 0 } },
+    points: { yes: 1, no: 0 }
+  },
   {
     category: "資金",
     text: "世帯年収は1000万円以上ですか？",
-    points: { yes: 7, no: 0 } },
+    points: { yes: 1, no: 0 }
+  },
   {
     category: "資金",
     text: "特に大きな出費がない月は毎月5万円以上の貯蓄はできますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "資金",
     text: "いまの収入は安定していますか？",
-    points: { yes: 7, no: 0 } },
+    points: { yes: 1, no: 0 }
+  },
   {
     category: "資金",
     text: "職場が潰れても一年以内には収入に戻せますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "自分",
     text: "あなたは金融について詳しいですか？",
-    points: { yes: 7, no: 0 } },
+    points: { yes: 1, no: 0 }
+  },
   {
     category: "自分",
     text: "投資や保険に関する知識を自ら身につけたいと思いますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "自分",
     text: "資産運用中の大きな下落にも冷静に対応できますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "自分",
     text: "ライフステージの変化に合わせ、資産を見直すことができますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "自分",
     text: "全て自分で解決したいと思いますか？",
-    points: { yes: 7, no: 0 } },
+    points: { yes: 1, no: 0 }
+  },
   {
     category: "リスク",
     text: "リスクはあっても早く大きく資産が増える投資に魅力を感じますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "リスク",
     text: "100倍になる可能性がある投資は魅力的ですか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "リスク",
     text: "投資で損失が出ても自己責任で受け入れられますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "リスク",
     text: "投資に失敗しても取り返すチャンスがあると思いますか？",
-    points: { yes: 7, no: 0 },
+    points: { yes: 1, no: 0 },
   },
   {
     category: "リスク",
     text: "自分の資産を客観的に扱えますか？",
-    points: { yes: 7, no: 0 }
+    points: { yes: 1, no: 0 }
   },
 ];
 
